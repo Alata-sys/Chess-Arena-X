7540ae624d913bf811b69f419baae10b8bd6c1db
Alata-sys <elysedesignpro@gmail.com>
2026-02-06 17:48:15 +0300

    gladiator.js

---
+++ js/gladiator.js
@@ -0,0 +1,9 @@
+console.log("Gladiator prêt !");
+document.getElementById('game-area').innerHTML = "<p>Ici sera ton échiquier du mode Gladiator.</p>";
+
+// Placeholder pour tester le jeu
+function startGladiator() {
+    alert("Gladiator : prépare les malus et le brouillard de guerre !");
+}
+
+startGladiator();
