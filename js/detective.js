aee9ed4e2ec941aedea0587a4f2d8ac8f545ff73
Alata-sys <elysedesignpro@gmail.com>
2026-02-06 17:45:44 +0300

    detective.js

---
+++ js/detective.js
@@ -0,0 +1,9 @@
+hereconsole.log("Detective prêt !");
+document.getElementById('game-area').innerHTML = "<p>Ici sera ton échiquier du mode Detective.</p>";
+
+// Placeholder pour tester le jeu
+function startDetective() {
+    alert("Detective : prépare les positions perdues et limite les crédits d'assistance !");
+}
+
+startDetective();
