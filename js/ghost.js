62c697c41f3d4ffcce77b47520c406f05d88ff01
Alata-sys <elysedesignpro@gmail.com>
2026-02-06 17:41:54 +0300

    ghost.js

---
+++ js/ghost.js
@@ -0,0 +1,9 @@
+console.log("Ghost Master prêt !");
+document.getElementById('game-area').innerHTML = "<p>Ici sera ton échiquier Boss Challenge.</p>";
+
+// Exemple : placeholder pour un Boss Challenge
+function startGhostMaster() {
+    alert("Ghost Master : prépare le Boss et les défis !");
+}
+
+startGhostMaster();
